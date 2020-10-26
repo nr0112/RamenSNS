e3fc495ba07af292759597489ddc5dcfcc343d7a
nr0112 <ririho0112@gmail.com>
2020-10-26 02:33:29 +0000

    changed

--- resources/views/posts/myposts.blade.php
+++ resources/views/posts/myposts.blade.php
@@ -12,9 +12,9 @@
         @foreach($posts as $post)
             <div class="post-content">
                 <a class="post-link" href="{{ route('posts.detail', ['post_id'=>$post->id])}}">
-                    <img class="post-photo img-thumbnail" src="{{ seccure_asset('storage/photos/'.$post->filename) }}" alt="投稿した画像">
+                    <img class="post-photo img-thumbnail" src="{{ secure_asset('storage/photos/'.$post->filename) }}" alt="投稿した画像">
                     <div class="post-info">
-                        <img class="info-user-image" src="{{ seccure_asset('storage/user_images/'.$post->user->user_image) }}">
+                        <img class="info-user-image" src="{{ secure_asset('storage/user_images/'.$post->user->user_image) }}">
                         <p class="info-user-name"><span style="font-size: 20px; font-weight: 900;">{{ $post->content }}</span> ({{ $post->created_at }})</p>
                     </div>
                 </a>
